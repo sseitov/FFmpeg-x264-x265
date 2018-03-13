60023221ada9835a04fe89c54ba7a442ca920b32
Sergey Seitov <sseitov@gmail.com>
2018-03-13 21:55:57 +0300

    init

---
+++ FFmpeg-iOS/include/x265_config.h
@@ -0,0 +1,15 @@
+//
+//  x265_config.h
+//  x265foriOS
+//
+//  Created by Ruiwen Feng on 2017/5/27.
+//  Copyright © 2017年 Ruiwen Feng. All rights reserved.
+//
+
+#ifndef x265_config_h
+#define x265_config_h
+
+#define X265_BUILD 116
+
+
+#endif /* x265_config_h */
