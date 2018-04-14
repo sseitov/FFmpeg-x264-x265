9f0759617b688e0369a38fd2aa84c92b68502757
Sergey Seitov <sseitov@gmail.com>
2018-04-14 21:26:28 +0300

    x265 fix

--- x265foriOS/include/x265_config.h
+++ x265foriOS/include/x265_config.h
@@ -1,34 +1,15 @@
-/*****************************************************************************
- * Copyright (C) 2013-2017 MulticoreWare, Inc
- *
- * Authors: Steve Borho <steve@borho.org>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
- *
- * This program is also available under a commercial proprietary license.
- * For more information, contact us at license @ x265.com.
- *****************************************************************************/
+//
+//  x265_config.h
+//  x265foriOS
+//
+//  Created by Ruiwen Feng on 2017/5/27.
+//  Copyright © 2017年 Ruiwen Feng. All rights reserved.
+//
 
-#ifndef X265_CONFIG_H
-#define X265_CONFIG_H
+#ifndef x265_config_h
+#define x265_config_h
 
-/* Defines generated at build time */
+#define X265_BUILD 116
 
-/* Incremented each time public API is changed, X265_BUILD is used as
- * the shared library SONAME on platforms which support it. It also
- * prevents linking against a different version of the static lib */
-#define X265_BUILD 156
 
-#endif
+#endif /* x265_config_h */
